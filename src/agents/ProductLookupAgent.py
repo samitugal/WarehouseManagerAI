1bdda2df3323f2d500c64b041667054707c38899
Sami Tugal <Sami.Tugal@logo.com.tr>
2024-07-31 20:31:59 +0300

    Agent refactor

--- src/agents/ProductLookupAgent.py
+++ src/agents/ProductLookupAgent.py
@@ -22,6 +22,25 @@ class ProductLookupAgent:
         self.llm = self._initialize_llm()
         self.prompt_template = self._load_prompt_template()
         self.agent_executor = self._create_agent_executor()
+        self.tools_for_agent = [
+            Tool(
+                name="Search for information about product in embeddings.",
+                func=get_product_information_from_embeddings,
+                description="""
+                    Searches for information about embeddings and provides information or description which helps general information
+                    about product origin, ingredients, etc..
+                    """,
+            ),
+            Tool(
+                name="Search Product database to get information like product_id, product_name, supplier_name, category_name, quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued",
+                func=get_product_information_from_db,
+                description="""
+                Useful for when you need to get information about a product in inventory. Information like product_id, product_name, 
+                supplier_name, category_name, quantity_per_unit, unit price, units in stock, units on order, reorder level, and discontinued status.
+                => To use this function, only pass the product name as a string.
+                """,
+            )
+        ]
 
     def _initialize_llm(self):
         cfg = LLMMainConfig.from_file(os.getenv("LLM_CONFIG_PATH"))
@@ -44,36 +63,18 @@ class ProductLookupAgent:
     def _load_prompt_template(self):
         template = load_prompt("agent_prompt_template")
         return PromptTemplate(
-            template=template, input_variables=["input", "chat_history"]
+            template=template, input_variables=["input", "chat_history","tools"]
         )
 
     def _create_agent_executor(self):
-        tools_for_agent = [
-            Tool(
-                name="Searche for information about product in embeddings.",
-                func=get_product_information_from_embeddings,
-                description="""
-                    Searches for information about embeddings and provides information or description which helps general information
-                    about product origin, ingredients, etc..
-                    """,
-            ),
-            Tool(
-                name="Search Product database to get information like product_id, product_name, supplier_name, category_name, quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued",
-                func=get_product_information_from_db,
-                description="""
-                Useful for when you need to get information about a product in inventory. Information like product_id, product_name, 
-                supplier_name, category_name, quantity_per_unit, unit price, units in stock, units on order, reorder level, and discontinued status.
-                => To use this function, only pass the product name as a string.
-                """,
-            )
-        ]
+
         react_prompt = hub.pull("hwchase17/react")
-        agent = create_react_agent(llm=self.llm, tools=tools_for_agent, prompt=react_prompt)
-        return AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+        agent = create_react_agent(llm=self.llm, tools=self.tools_for_agent, prompt=react_prompt)
+        return AgentExecutor(agent=agent, tools=self.tools_for_agent, verbose=True)
 
     def lookup(self, query: str, chat_history: List[Dict[str, Any]] = []) -> str:
         try:
-            formatted_input = self.prompt_template.format_prompt(input=query, chat_history=chat_history)
+            formatted_input = self.prompt_template.format_prompt(input=query, chat_history=chat_history, tools=self.tools_for_agent)
             result = self.agent_executor.invoke(input={"input": formatted_input})
             return result["output"]
         except Exception as e:
